8db9973e2c978ce33a48104b38f22178a6bb5309
TheJuanRojasR <juan_mrojasr@soy.sena.edu.co>
2025-03-27 16:09:19 -0500

    Clase 3
    Se crea un servidor utilizando express.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 'use strict';
 
-console.log('My app');
+
+import express from 'express'; // Traemos el paquete express
+const app = express();             // Creamos una instancia de express
+const PORT = 3000;                 // Definimos el puerto en el que se ejecutará el servidor
+
+app.get('/', ( req, res ) => {
+    res.send('Hola Mundo');
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
